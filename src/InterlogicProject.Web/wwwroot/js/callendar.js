d347287fa0b6cfba68893797862ccba05de37488
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2016-12-20 22:47:56 +0200

    Added loading the classes for the current week

--- src/InterlogicProject.Web/wwwroot/js/callendar.js
+++ src/InterlogicProject.Web/wwwroot/js/callendar.js
@@ -1,37 +1,22 @@
-$(document).ready(() => {
+$(document).ready(function () {
+    var classes = getClasses(moment().startOf("week"), moment().endOf("week"));
+    var events = classes.map(classToEvent);
     $("#calendar").fullCalendar({
         allDaySlot: false,
         defaultView: "agendaWeek",
-        eventClick: () => {
-            $.get("http://localhost:8000/api/users/current", (data) => {
-                let displayData = "";
-                for (let key in data) {
-                    if (data.hasOwnProperty(key)) {
-                        displayData += `${key}: ${data[key]}\n`;
-                    }
-                }
-                alert(`Current user:\n${displayData}`);
-            });
-        },
+        eventClick: eventClicked,
+        eventColor: "#0275D8",
+        events: events,
+        weekends: false,
+        weekNumbers: true
+    });
+    /*{
+        allDaySlot: false,
+        defaultView: "agendaWeek",
+        eventClick: eventClicked,
         eventColor: "#0275d8",
         eventLimit: 5,
-        events: [
-            {
-                title: "Подія 1",
-                start: "2016-12-19T10:00",
-                end: "2016-12-19T12:00"
-            },
-            {
-                title: "Подія 2",
-                start: "2016-12-20T12:00",
-                end: "2016-12-20T13:30"
-            },
-            {
-                title: "Подія 3",
-                start: "2016-12-20T15:00",
-                end: "2016-12-20T17:00"
-            }
-        ],
+        events: events,
         header: {
             left: "title",
             right: "today,month,agendaDay,agendaWeek prev,next"
@@ -42,6 +27,47 @@ $(document).ready(() => {
         selectable: true,
         weekends: false,
         weekNumbers: true
-    });
+    });*/
 });
+function getClasses(start, end) {
+    "use strict";
+    var result = [];
+    $.get({
+        url: "http://localhost:8000/api/classes/range/" +
+            (start.format("YYYY-MM-DD") + "/" + end.format("YYYY-MM-DD")),
+        async: false,
+        success: function (data) {
+            for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+                var item = data_1[_i];
+                result.push(item);
+            }
+        },
+        error: function () { return true; }
+    });
+    return result;
+}
+function eventClicked() {
+    "use strict";
+    $.get({
+        url: "http://localhost:8000/api/users/current",
+        async: true,
+        success: function (data) {
+            var displayData = "";
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    displayData += key + ": " + data[key] + "\n";
+                }
+            }
+            alert("Current user:\n" + displayData);
+        }
+    });
+}
+function classToEvent(classInfo) {
+    "use strict";
+    return {
+        title: classInfo.subjectName + ": " + classInfo.type,
+        start: classInfo.dateTime,
+        end: moment.utc(classInfo.dateTime).add(1, "hours").add(20, "minutes").format()
+    };
+}
 //# sourceMappingURL=callendar.js.map
--- src/InterlogicProject.Web/wwwroot/js/callendar.ts
+++ src/InterlogicProject.Web/wwwroot/js/callendar.ts
@@ -1,43 +1,28 @@
-﻿declare var $: any;
+﻿declare var moment: any;
 
 $(document).ready(() => {
+	const classes = getClasses(
+		moment().startOf("week"),
+		moment().endOf("week"));
+
+	const events = classes.map(classToEvent);
+
 	$("#calendar").fullCalendar({
 		allDaySlot: false,
 		defaultView: "agendaWeek",
-		eventClick: () => {
-			$.get(
-				"http://localhost:8000/api/users/current",
-				(data: any) => {
-					let displayData = "";
-
-					for (let key in data) {
-						if (data.hasOwnProperty(key)) {
-							displayData += `${key}: ${data[key]}\n`;
-						}
-					}
-
-					alert(`Current user:\n${displayData}`);
-				});
-        },
+		eventClick: eventClicked,
+		eventColor: "#0275D8",
+		events: events,
+		weekends: false,
+		weekNumbers: true
+	});
+	/*{
+		allDaySlot: false,
+		defaultView: "agendaWeek",
+		eventClick: eventClicked,
 		eventColor: "#0275d8",
 		eventLimit: 5,
-		events: [
-			{
-				title: "Подія 1",
-				start: "2016-12-19T10:00",
-				end: "2016-12-19T12:00"
-			},
-			{
-				title: "Подія 2",
-				start: "2016-12-20T12:00",
-				end: "2016-12-20T13:30"
-			},
-			{
-				title: "Подія 3",
-				start: "2016-12-20T15:00",
-				end: "2016-12-20T17:00"
-			}
-		],
+		events: events,
 		header: {
 			left: "title",
 			right: "today,month,agendaDay,agendaWeek prev,next"
@@ -48,5 +33,54 @@ $(document).ready(() => {
 		selectable: true,
 		weekends: false,
 		weekNumbers: true
-	});
+	});*/
 });
+
+function getClasses(start: any, end: any): any[] {
+	"use strict";
+
+	const result: any[] = [];
+
+	$.get({
+		url: "http://localhost:8000/api/classes/range/" +
+			 `${start.format("YYYY-MM-DD")}/${end.format("YYYY-MM-DD")}`,
+		async: false,
+		success: (data: any) => {
+			for (let item of data) {
+				result.push(item);
+			}
+		},
+		error: () => { return true; }
+	});
+
+	return result;
+}
+
+function eventClicked(): void {
+	"use strict";
+
+	$.get({
+		url: "http://localhost:8000/api/users/current",
+		async: true,
+		success: (data: any) => {
+			let displayData = "";
+
+			for (let key in data) {
+				if (data.hasOwnProperty(key)) {
+					displayData += `${key}: ${data[key]}\n`;
+				}
+			}
+
+			alert(`Current user:\n${displayData}`);
+		}});
+}
+
+function classToEvent(classInfo: any): any {
+	"use strict";
+
+	return {
+		title: `${classInfo.subjectName}: ${classInfo.type}`,
+		start: classInfo.dateTime,
+		end: moment.utc(classInfo.dateTime).add(1, "hours").add(20, "minutes").format()
+	};
+}
